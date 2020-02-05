1d01cd1e7764bbb1622a968618e8ee904aae8aed
kmolchan <zebraside@gmail.com>
2020-02-06 01:29:28 +0300

    add configuration file

--- generate.sh
+++ generate.sh
@@ -4,5 +4,5 @@ cd ..
 
 mkdir build || exit
 cd build || return
-cmake -DCMAKE_PREFIX_PATH=/home/runner/work/GlobalOptimization/GlobalOptimization/Thirdparty/Qt/lib/cmake  -DENABLE_GUI=ON ..
+cmake -DENABLE_GUI=OFF ..
 make
--- unpack.sh
+++
@@ -1,2 +0,0 @@
-cd Thirdparty
-unzip Qt.zip
