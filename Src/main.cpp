7e4db67b4f3087e884b3af26aaeece7cbfdb53bc
kmolchan <zebraside@gmail.com>
2020-02-05 21:49:46 +0300

    Add basic plot example

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,3 +1,10 @@
-int main() {
+#include <QtWidgets/QApplication>
+
+#include "Presenters/main_window.hpp"
+
+int main(int argc, char **argv) {
+    QApplication app(argc, argv);
+    MainWinow window;
+    return app.exec();
     return 0;
 }
