bdf1656203992b1e29ed1dff6b786c1e9f9cc552
akharche <angelina.kharchevnikova@intel.com>
2020-03-09 19:52:42 +0300

    Add first version of Uniform Search Method

---
+++ Src/Optimization/UniformSearchMethod.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "UniformSearchMethod.h"
+#include <algorithm>
+
+void UniformSearchMethod::performTest(Function& f, double a, double b, double epsilon, int n)
+{
+	testHistory.push_back(Test(a+idx*(b-a)/n,f(a+idx*(b-a)/n)));
+	idx++;
+}
+void UniformSearchMethod::evaluateSolution()
+{
+	std::sort(testHistory.begin(), testHistory.end(), compareByValue);
+	solution=testHistory[0].point;
+}
+bool UniformSearchMethod::checkStopCriterion(double epsilon, int n)
+{
+	return idx>n;
+}
+
+UniformSearchMethod::UniformSearchMethod()
+{
+	idx=0;
+}
